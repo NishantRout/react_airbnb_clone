9cfdca7d30490d24cb0778278857efafcebd9ed3
NishantRout <nishantnarayanrout@gmail.com>
2020-10-09 17:27:51 +0530

    setting up react-router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import SearchPage from "./components/SearchPage";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
