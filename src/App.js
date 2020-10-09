80a2d1c7c3b370bbcb8e892a7004ffabd121d3df
NishantRout <nishantnarayanrout@gmail.com>
2020-10-09 17:07:03 +0530

    footer component ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
